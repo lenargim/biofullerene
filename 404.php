2df38525a6b3f08578abf052c088e68578e2d02e
lenargim <linz14@mail.ru>
2022-10-09 02:57:31 +0400

    Not-found page

--- 404.php
+++ 404.php
@@ -1,18 +1,28 @@
-<?php get_template_part('template-parts/header/head'); ?>
-<body>
-<div class="not-found">
-  <div class="not-found__box">
-    <div class="not-found__head">404</div>
-    <div class="not-found__oops">Упс!</div>
-    <div class="not-found__span">Ошибочка</div>
-    <div class="not-found__info">
-      <div class="not-found__info-item">Вернуться на <a href="/">главную страницу</a></div>
-      <div class="not-found__info-item">Или позвоните нам
-        <a href="tel:<?php echo preg_replace('/[' . PHONE_CHARS . ']/', '', get_field('phone', 38)) ?>">
-          <?php echo get_field('phone', 38) ?>
-        </a>
-      </div>
-    </div>
-  </div>
-</div>
-</body>
+<?php get_template_part( 'template-parts/header/head' ); ?>
+
+    <main class="not-found">
+        <div class="not-found__top">
+            <div class="container">
+                <a href="/" class="not-found__logo">
+                    <svg>
+                        <use xlink:href="<?php echo IMAGES_PATH; ?>/sprite-common.svg#logo"></use>
+                    </svg>
+                </a>
+            </div>
+        </div>
+        <div class="not-found__bottom">
+            <div class="container">
+                <div class="not-found__text">
+                    <div class="not-found__title"><span class="underline">Oops!</span><br>Page Not Found</div>
+                    <div class="not-found__desc">Looks like this page doesn't exist, sorry. That button below seems
+                        pretty
+                        important right about now.
+                    </div>
+                    <a href="/" class="not-found__back button white">Back to homepage</a>
+                </div>
+            </div>
+        </div>
+        </div>
+    </main>
+
+<?php get_template_part( 'template-parts/footer/footer' ); ?>
